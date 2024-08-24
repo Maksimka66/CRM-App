d219fb59914f8c74b0aa4a0c5642a37d192ee4ef
Maksimka66 <maxfese@gmail.com>
2024-08-24 18:31:44 +0300

    some bugs fixed

--- crm/src/app/components/input-field.tsx
+++ crm/src/app/components/input-field.tsx
@@ -3,7 +3,8 @@
 import React from 'react';
 import { Field } from 'formik';
 
-export interface InputFieldProps {
+export interface InputFieldProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
