5dd55a90db66c2984baa31541bd8c2dd81a40772
Maksimka66 <maxfese@gmail.com>
2024-08-28 04:52:31 +0300

    added dynamic routing

--- crm/src/app/companies/page.tsx
+++ crm/src/app/companies/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from '../components/header';
 import Toolbar from '../components/toolbar';
 import SearchInput from '../components/search-input';
+import AddCompanyButton from '../components/add-company-button';
+import CompanyTable from '../components/company-table';
+import CompanyRow from '../components/company-row';
+import { Status } from '../components/status-label';
 
 export interface PageProps {}
 
@@ -9,9 +13,20 @@ export default function Page({}: PageProps) {
   return (
     <>
       <Header>Companies</Header>
-      <Toolbar>
+      <Toolbar action={<AddCompanyButton />}>
         <SearchInput />
       </Toolbar>
+      <CompanyTable>
+        <CompanyRow
+          id={1}
+          category={'Products'}
+          company={'Costco'}
+          country={'USA'}
+          joinedDate={'02.19.2023'}
+          promotion={true}
+          status={Status.Pending}
+        />
+      </CompanyTable>
     </>
   );
 }
