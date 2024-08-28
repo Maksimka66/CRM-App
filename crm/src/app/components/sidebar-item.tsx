6e8ec2811f17557ca9ce37040894469f4516d82c
Maksimka66 <maxfese@gmail.com>
2024-08-28 18:09:33 +0300

    work with sidebar

--- crm/src/app/components/sidebar-item.tsx
+++ crm/src/app/components/sidebar-item.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
 
-export interface SidebarItemProps {}
+export interface SidebarItemProps {
+  children: React.ReactNode;
+  pathname: string;
+  src: string;
+  alt: string;
+}
 
-export default function SidebarItem({}: SidebarItemProps) {
-  return;
+export default function SidebarItem({
+  children,
+  pathname,
+  src,
+  alt,
+}: SidebarItemProps) {
+  return (
+    <li>
+      <a href={pathname}>
+        <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
+        <span className="font-medium text-zinc-50">{children}</span>
+      </a>
+    </li>
+  );
 }
--- crm/src/app/components/sidebar.tsx
+++ crm/src/app/components/sidebar.tsx
@@ -4,5 +4,5 @@ import Image from 'next/image';
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
-  return;
+  return <aside className="fixed top-0 left-0 z-40 w-60 h-screen"></aside>;
 }
